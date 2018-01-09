6936d8ffa4c5ec872cbf023ca13e3d6ce1a17fb2
Adam Smith and Tyler Bussanich <tyler@>
2018-01-08 16:32:43 -0800

    add more stuff

--- spec/cat-spec.js
+++ spec/cat-spec.js
@@ -1,21 +1,26 @@
-import { Cat } from './../src/js/cat.js'
+import { cat } from './../src/js/cat.js'
 
 describe('Cat', function() {
-  let badCat = new Cat("Ferguson");
+  let badCat = cat;
 
   beforeEach(function() {
     jasmine.clock().install();
     badCat.setHunger();
+    badCat.name = "Ferguson";
+    badCat.hungerLevel = 0;
+    badCat.disobeyLevel = 5;
+    badCat.entertainLevel = 5;
   });
 
   afterEach(function() {
     jasmine.clock().uninstall();
   })
 
-  it('should start with a name, hunger level of 0, and a behavior level of 5', function() {
-    expect(badCat.hamSandwich).toEqual("Ferguson");
+  it('should start with a name, hunger level of 0, disobey level of 5, and entertainment level of 5', function() {
+    expect(badCat.name).toEqual("Ferguson");
     expect(badCat.hungerLevel).toEqual(0);
-    expect(badCat.behaviorLevel).toEqual(5);
+    expect(badCat.disobeyLevel).toEqual(5);
+    expect(badCat.entertainLevel).toEqual(5);
   });
 
   it('should have a hunger level of 3 after 30001 milliseconds', function() {
@@ -23,7 +28,42 @@ describe('Cat', function() {
     expect(badCat.hungerLevel).toEqual(3);
   });
 
-  it('should have a hunger level of 0 if it is fed wet food', function () {
-    expect(badCat.hungerLevel).toEqual(0);
-  })
+  it('should return that the cat ate Dried Minnows and the hunger level went down 2', function() {
+    jasmine.clock().tick(70001);
+    expect(badCat.feedTreat('Dried Minnows')).toEqual("You fed Ferguson some Dried Minnows! Hunger level goes down 2!");
+    expect(badCat.hungerLevel).toEqual(5);
+  });
+
+  it('should return that the cat ate Kibble and the hunger level went down 3', function() {
+    jasmine.clock().tick(70001);
+    expect(badCat.feedDryFood('Kibble')).toEqual('You fed Ferguson some Kibble! Hunger level goes down 3!');
+    expect(badCat.hungerLevel).toEqual(4);
+  });
+
+  it('should return that the cat ate Fancy Feast and the hunger level went down 5', function() {
+    jasmine.clock().tick(70001);
+    expect(badCat.feedWetFood('Fancy Feast')).toEqual('You fed Ferguson some Fancy Feast! Hunger level goes down 5!');
+    expect(badCat.hungerLevel).toEqual(2);
+  });
+
+  it('should return that the cat ate Lasagna and the hunger level goes down 3, but also gives a message that it isn\'t normal for cats to eat that', function() {
+    jasmine.clock().tick(70001);
+    expect(badCat.feedPeopleFood('Lasagna')).toEqual("You fed Ferguson some Lasagna. Hunger level goes down 3, but it isn't normal for cats to eat that! Disobey level increases 2!");
+    expect(badCat.disobeyLevel).toEqual(7);
+    expect(badCat.hungerLevel).toEqual(4);
+  });
+
+  it('should return that you played Mouse Toy with the cat. Entertainment level goes up 2, hunger level goes up 1, and disobey level drops 1.', function() {
+    expect(badCat.playGameSmall('Mouse Toy')).toEqual("You played Mouse Toy with Ferguson! Entertainment level goes up 2 and Ferguson bats his eyes at you in admiration.");
+    expect(badCat.entertainLevel).toEqual(7);
+    expect(badCat.hungerLevel).toEqual(1);
+    expect(badCat.disobeyLevel).toEqual(4);
+  });
+
+  it('should return that you played Laser Pointer with the cat. Entertainment level goes up 4, hunger level goes up 2, and disobey level drops 2', function() {
+    expect(badCat.playGameBig('Laser Pointer')).toEqual("You played Laser Pointer with Ferguson! Entertainment level goes up 4 and Ferguson purs loudly while rolling around on the floor like a dum-dum.");
+    expect(badCat.entertainLevel).toEqual(9);
+    expect(badCat.hungerLevel).toEqual(2);
+    expect(badCat.disobeyLevel).toEqual(3);
+  });
 });
--- src/js/cat.js
+++ src/js/cat.js
@@ -1,25 +1,65 @@
-export class Cat {
-  constructor(name) {
-    this.hamSandwich = name;
-    this.hungerLevel = 0;
-    this.behaviorLevel = 5;
-  }
+export let cat = {
 
-  setHunger() {
-    setInterval(() => {
+  setHunger: function() {
+  const hungerInterval =  setInterval(() => {
       this.hungerLevel++;
     }, 10000);
-  }
+  },
 
-  feedCat(amount) {
-    return function(food) => {
-      this.hungerLevel -= amount
-      return `You fed ${this.hamSandwich} ${food}! Hunger level goes down ${amount}!`
+  setBoredom: function() {
+    const boredomInterval = setInterval(() => {
+      this.entertainLevel--;
+    }, 10000);
+  },
+
+  setDisobey: function() {
+    let interval = 10000;
+    let disobeyInterval = setInterval(() => {
+      const disobeyChance = this.disobeyLevel * 5;
+      if (Math.ceil(Math.random() * 100) < disobeyChance) {
+        this.catDisobey();
+      };
+    }, interval);
+  },
+
+  feedCat: function(amount) {
+    let that = this;
+    return function(food) {
+      that.hungerLevel -= amount
+      return `You fed ${that.name} some ${food}! Hunger level goes down ${amount}!`
+    }
+  },
+
+  peopleFood: function(amount) {
+    let that = this;
+    return function(food) {
+      that.disobeyLevel += 2;
+      that.hungerLevel -= amount;
+      return `You fed ${that.name} some ${food}. Hunger level goes down ${amount}, but it isn't normal for cats to eat that! Disobey level increases 2!`
+    }
+  },
+
+  entertainCat: function(amount, gameType) {
+    let that = this;
+    return function(game) {
+      that.entertainLevel += amount;
+      if (gameType === "small") {
+        that.hungerLevel += 1;
+        that.disobeyLevel -= 1;
+        return `You played ${game} with ${that.name}! Entertainment level goes up ${amount} and ${that.name} bats his eyes at you in admiration.`
+      }
+      else if (gameType === "big") {
+        that.hungerLevel += 2;
+        that.disobeyLevel -= 2;
+        return `You played ${game} with ${that.name}! Entertainment level goes up ${amount} and ${that.name} purs loudly while rolling around on the floor like a dum-dum.`
+      }
     }
   }
 }
 
-Cat.eatTreat = Cat.feedCat(2);
-Cat.eatDryFood = cat.feedCat(3);
-Cat.eatWetFood = cat.feedCat(5);
-Cat.eatPeopleFood = cat.feedCat(3);
+cat.feedTreat = cat.feedCat(2);
+cat.feedDryFood = cat.feedCat(3);
+cat.feedWetFood = cat.feedCat(5);
+cat.feedPeopleFood = cat.peopleFood(3);
+cat.playGameSmall = cat.entertainCat(2, "small");
+cat.playGameBig = cat.entertainCat(4, "big");
